8a76a4bb8c70f6c95af4eaeb507934a24be17485
Ahmet Sefa Arşiv <ahmetarsivpm@gmail.com>
2024-06-05 12:48:33 +0000

    UI upgrade

--- resources/js/Layouts/MainLayout.jsx
+++ resources/js/Layouts/MainLayout.jsx
@@ -1,16 +1,13 @@
 import {Link} from "@inertiajs/react";
 import {Toaster} from "react-hot-toast";
 
-const MainLayout = ({ children }) => {
+const MainLayout = ({children}) => {
     return (
         <div>
-            <Toaster />
-            <header className="bg-gray-800 text-white p-4">
+            <Toaster/>
+            <header className="fixed w-full backdrop-blur-md border-b border-zinc-100 px-4 lg:px-6 py-2.5 bg-white/70 z-10">
                 <div className="container mx-auto flex justify-between items-center">
-                    <Link href="/public" className="text-lg font-bold">Eventeq</Link>
-                    <nav>
-                        <Link href="/public" className="mr-4">Home</Link>
-                    </nav>
+                    <Link href="/" className="text-2xl font-bold uppercase">Eventeq</Link>
                 </div>
             </header>
 
@@ -18,20 +15,16 @@ const MainLayout = ({ children }) => {
                 {children}
             </main>
 
-            <footer className="text-gray-600 body-font">
+            <footer className="text-zinc-600 body-font">
                 <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-                    <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round"
-                             strokeLinejoin="round" strokeWidth="2"
-                             className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-                            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-                        </svg>
+                    <a className="flex title-font font-medium items-center md:justify-start justify-center text-zinc-900">
                         <span className="ml-3 text-xl">Codenteq</span>
                     </a>
-                    <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
-                        &#169; 2024.
+                    <p className="text-sm text-zinc-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-zinc-200 sm:py-2 sm:mt-0 mt-4">
+                        &#169; 2021-2024 Codenteq.
                     </p>
-                    <a href="https://codenteq.com" className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+                    <a href="https://codenteq.com"
+                       className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
                         Codenteq.com
                     </a>
                 </div>
--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,16 +1,41 @@
 import {Link} from "@inertiajs/react";
 import {Button} from "@codenteq/interfeys";
 import MainLayout from "../Layouts/MainLayout.jsx";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import {Navigation, Pagination} from "swiper/modules";
+import {HeartIcon, StarIcon} from "@heroicons/react/24/outline/index.js";
 
-export default function Welcome({events}) {
-    console.log(events)
+export default function Welcome({ events }) {
+    const slides = [
+        { image_url: '/slide01.png' },
+        { image_url: '/slide02.png' },
+    ];
+
     return (
         <>
             <MainLayout>
-                <section className="text-gray-600 body-font">
+                <section className="container mx-auto py-10">
+                    <Swiper
+                        pagination={{clickable: false}}
+                        navigation={false}
+                        modules={[Pagination, Navigation]}
+                        className="mySwiper"
+                    >
+                        {slides.map((slide, index) => (
+                            <SwiperSlide key={index}>
+                                <img src={slide.image_url} alt={`Slide ${index + 1}`}/>
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                </section>
+
+                <section className="text-zinc-600 body-font">
                     <div className="container px-5 py-24 mx-auto">
                         <div className="text-center w-full mb-10">
-                            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+                            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-zinc-900">
                                 Öne Çıkan Etkinlikler
                             </h1>
                         </div>
@@ -18,19 +43,19 @@ export default function Welcome({events}) {
                             {events.map((event) => (
                                 <div
                                     key={event.id}
-                                    className="flex flex-col max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                                    className="flex flex-col max-w-sm bg-white border border-zinc-200 rounded-lg shadow">
                                     <Link href={`/events/${event.id}`} className="flex-shrink-0">
                                         <img className="rounded-t-lg w-full" src={`storage/${event.img}`}
                                              alt={event.name}/>
                                     </Link>
                                     <div className="flex flex-col flex-grow p-5">
                                         <Link href={`/events/${event.id}`} className="mb-2">
-                                            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                            <h5 className="text-2xl font-bold tracking-tight text-zinc-900">
                                                 {event.name}
                                             </h5>
                                         </Link>
                                         <div className="flex-grow">
-                                            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                                            <p className="mb-3 font-normal text-zinc-700">
                                                 📍{event?.city?.name}
                                             </p>
                                         </div>
@@ -40,6 +65,51 @@ export default function Welcome({events}) {
                                     </div>
                                 </div>
                             ))}
+                        </div>
+                    </div>
+                </section>
+
+                <section className="grid grid-cols-1 md:grid-cols-2 gap-5">
+                    <div
+                        className="relative bg-cover bg-[url('https://kommunity.com/img/cta1.jpg')] w-full h-80 md:h-full">
+                        <div className="absolute inset-0 bg-black opacity-50"></div>
+                        <div
+                            className="relative flex flex-col items-center md:items-start justify-center px-5 py-7 h-full">
+                            <StarIcon className="w-14 h-14 lg:w-22 lg:h-22 text-white mb-4"/>
+                            <h3 className="text-xl text-white md:text-2xl font-semibold mb-2">
+                                Codenteq'i ziyaret edin
+                            </h3>
+                            <p className="text-sm text-zinc-300 md:text-base">
+                                {'Açık kaynak topluluğumuzu ziyaret edin.'}
+                            </p>
+                            <a
+                                className="text-sm text-white md:text-base underline mt-2"
+                                href="https://codenteq.com"
+                                target="_blank"
+                            >
+                                Ziyaret et
+                            </a>
+                        </div>
+                    </div>
+                    <div
+                        className="relative bg-cover bg-[url('https://kommunity.com/img/cta2.jpg')] w-full h-80 md:h-full">
+                        <div className="absolute inset-0 bg-black opacity-50"></div>
+                        <div
+                            className="relative flex flex-col items-center md:items-start justify-center px-5 py-7 h-full">
+                            <HeartIcon className="w-14 h-14 lg:w-22 lg:h-22 text-white mb-4"/>
+                            <h3 className="text-xl text-white md:text-2xl font-semibold mb-2">
+                                İmtihan
+                            </h3>
+                            <p className="text-sm text-zinc-300 md:text-base">
+                                {'Eğitim hayatınızı kolaylaştıran pratik bir platform.'}
+                            </p>
+                            <a
+                                className="text-sm text-white md:text-base underline mt-2"
+                                href="https://imtihantech.com"
+                                target="_blank"
+                            >
+                                Ziyaret et
+                            </a>
                         </div>
                     </div>
                 </section>
