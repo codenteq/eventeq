12152aec2a0d64d52b676e00a64b50d54f000393
Ali KARABAY <45515180+karabayyazilim@users.noreply.github.com>
2024-06-07 02:30:50 +0300

    mail translation added.

---
+++ lang/tr/mail.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+     * |--------------------------------------------------------------------------
+     * | Mail Language Lines
+     *
+     * |--------------------------------------------------------------------------
+     *
+     * | The following language lines are used by the mail system to build
+     * | the simple mail messages. You are free to change them to anything
+     * | you want to customize your views to better match your application.
+     *
+     */
+
+    'hello' => 'Merhaba!',
+    'regards' => 'Saygılarımızla',
+    'Regards' => 'Saygılarımızla',
+    'trouble' => 'Eğer ":actionText" butonuna tıklamakta sorun yaşıyorsanız, aşağıdaki URL\'yi kopyalayıp web tarayıcınızın adres çubuğuna yapıştırarak da işlemi tamamlayabilirsiniz:',
+    'all_rights_reserved' => 'Tüm hakları saklıdır.',
+
+];
