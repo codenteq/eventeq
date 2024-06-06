6204b02d3b3b8aa8e33650eef1f236f13eff0da9
Ali KARABAY <45515180+karabayyazilim@users.noreply.github.com>
2024-06-07 02:31:30 +0300

    phone unique removed.

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('phone')->unique()->nullable();
+            $table->string('phone')->nullable();
             $table->date('birth_date')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
