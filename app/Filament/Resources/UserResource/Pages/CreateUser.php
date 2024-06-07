b3ab78451262b7cabe0bd76a4b66879449121037
Ali KARABAY <45515180+karabayyazilim@users.noreply.github.com>
2024-06-07 14:15:50 +0300

    user create role added.

--- app/Filament/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -2,11 +2,20 @@
 
 namespace App\Filament\Resources\UserResource\Pages;
 
+use App\Enums\Role;
 use App\Filament\Resources\UserResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        $data['role'] = Role::Admin->value;
+
+        return static::getModel()::create($data);
+    }
 }
