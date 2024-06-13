bc523d18c5990c951ed0d105602a9cb8f491c2f5
Ali KARABAY <45515180+karabayyazilim@users.noreply.github.com>
2024-06-13 21:43:05 +0300

    info application id added.

--- resources/js/Pages/EventApplicationSuccess.jsx
+++ resources/js/Pages/EventApplicationSuccess.jsx
@@ -10,6 +10,9 @@ export default function EventApplicationSuccess({application}) {
                     <h2 className="text-xl font-semibold text-green-500 sm:text-2xl mb-2">
                         Başarılı bir şekilde kaydınız gerçekleştirildi
                     </h2>
+                    <h3 className="text-blue-400">
+                        Başvuru Numaranız : {application?.id}
+                    </h3>
                     <p className="text-blue-400">
                         Lütfen Başvuru numaranızı Check-in sırasında kullanmak saklayınız.  Size bu numara eposta ile de gönderilmiştir.
                     </p>
