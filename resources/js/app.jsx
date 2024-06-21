a088ae235fd8ed23f22f2e94eb7fdbe3d9e0cdb0
Ali KARABAY <45515180+karabayyazilim@users.noreply.github.com>
2024-06-22 01:24:43 +0300

    posthog edited.

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,22 +1,27 @@
-import { createInertiaApp } from '@inertiajs/react'
-import { createRoot } from 'react-dom/client'
+import {createInertiaApp} from '@inertiajs/react'
+import {createRoot} from 'react-dom/client'
 
-import posthog from 'posthog-js'
+import {PostHogProvider} from "posthog-js/react";
 
-posthog.init('phc_z4pthNYl2vICRVSlgdOKUfmCHCebsGI2bJDMFsnKvXc',
-    {
-        api_host: 'https://us.i.posthog.com',
-        person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
-    }
-)
+
+const options = {
+    api_host: 'https://us.i.posthog.com'
+}
 
 createInertiaApp({
     title: title => 'Eventeq',
     resolve: name => {
-        const pages = import.meta.glob("./Pages/**/*.jsx", { eager: true });
+        const pages = import.meta.glob("./Pages/**/*.jsx", {eager: true});
         return pages[`./Pages/${name}.jsx`]
     },
-    setup({ el, App, props }) {
-        createRoot(el).render(<App {...props} />)
+    setup({el, App, props}) {
+        createRoot(el).render(
+            <PostHogProvider
+                apiKey="phc_z4pthNYl2vICRVSlgdOKUfmCHCebsGI2bJDMFsnKvXc"
+                options={options}
+                >
+                <App {...props} />
+            </PostHogProvider>
+        )
     },
 })
