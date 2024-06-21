dda3ea6244a2548fb794cb8d50b4dd26fb416a67
Ali KARABAY <45515180+karabayyazilim@users.noreply.github.com>
2024-06-22 01:05:47 +0300

    posthog added.

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,6 +1,15 @@
 import { createInertiaApp } from '@inertiajs/react'
 import { createRoot } from 'react-dom/client'
 
+import posthog from 'posthog-js'
+
+posthog.init('phc_z4pthNYl2vICRVSlgdOKUfmCHCebsGI2bJDMFsnKvXc',
+    {
+        api_host: 'https://us.i.posthog.com',
+        person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
+    }
+)
+
 createInertiaApp({
     title: title => 'Eventeq',
     resolve: name => {
