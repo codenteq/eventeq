e573d188109922f1b86e63f8aefd4501e4e2312f
Ali KARABAY <45515180+karabayyazilim@users.noreply.github.com>
2024-06-22 01:30:09 +0300

    posthog edited.

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,25 +1,26 @@
-import {createInertiaApp} from '@inertiajs/react'
-import {createRoot} from 'react-dom/client'
-
+import { createInertiaApp } from '@inertiajs/react'
+import { createRoot } from 'react-dom/client'
 import {PostHogProvider} from "posthog-js/react";
+import posthog from 'posthog-js'
 
-
-const options = {
-    api_host: 'https://us.i.posthog.com'
-}
+posthog.init('phc_z4pthNYl2vICRVSlgdOKUfmCHCebsGI2bJDMFsnKvXc',
+    {
+        api_host: 'https://us.i.posthog.com',
+        person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
+    }
+)
 
 createInertiaApp({
     title: title => 'Eventeq',
     resolve: name => {
-        const pages = import.meta.glob("./Pages/**/*.jsx", {eager: true});
+        const pages = import.meta.glob("./Pages/**/*.jsx", { eager: true });
         return pages[`./Pages/${name}.jsx`]
     },
-    setup({el, App, props}) {
+    setup({ el, App, props }) {
         createRoot(el).render(
             <PostHogProvider
-                apiKey="phc_z4pthNYl2vICRVSlgdOKUfmCHCebsGI2bJDMFsnKvXc"
-                options={options}
-                >
+               client={posthog}
+            >
                 <App {...props} />
             </PostHogProvider>
         )
