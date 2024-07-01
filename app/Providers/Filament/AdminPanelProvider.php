11c24ff9c1331a2663c0a9a0e1393cfc43c4019c
Ali KARABAY <45515180+karabayyazilim@users.noreply.github.com>
2024-07-01 17:06:21 +0300

    spa added.

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -58,6 +58,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ]);
+            ])
+            ->spa();
     }
 }
