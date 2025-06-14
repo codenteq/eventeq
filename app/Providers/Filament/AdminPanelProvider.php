af9ace90bb6bb7b08f1d691abd039b8b3321fc19
Ali KARABAY <45515180+karabayyazilim@users.noreply.github.com>
2025-06-14 21:44:44 +0300

    Add SPA middleware call in AdminPanelProvider

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -61,6 +61,6 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ]);
+            ])->spa();
     }
 }
