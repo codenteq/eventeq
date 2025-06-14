641bea7b03de1d37e00c6a76626c2a5a4e83a5c4
Ali KARABAY <45515180+karabayyazilim@users.noreply.github.com>
2025-06-14 21:38:43 +0300

    Remove unnecessary SPA middleware call in AdminPanelProvider

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -61,7 +61,6 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ])
-            ->spa();
+            ]);
     }
 }
